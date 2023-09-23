59c65aba14f1ca0c588de729ed5c25079f2e9a9e
imranKhanYRT <imran.khan.yrt@gmail.com>
2023-09-23 12:32:23 +0600

    make the skeleton for UI

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      hey
+      <h1>Online Code Compiler</h1>
+      <textarea rows={20} cols={75}></textarea>
+      <br />
+      <button className='submit_btn'>Submit</button>
     </div>
   );
 }

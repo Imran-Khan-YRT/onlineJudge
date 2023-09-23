74e9cd54d96b4e03c438efad06fca27fa740cdd0
imranKhanYRT <imran.khan.yrt@gmail.com>
2023-09-23 12:38:26 +0600

    add textarea for UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [code, setCode] = useState("");
+  const handleSubmit = () => {
+    console.log(code);
+  }
   return (
     <div className="App">
       <h1>Online Code Compiler</h1>
-      <textarea rows={20} cols={75}></textarea>
+      <textarea rows={20} cols={75} value={code} onChange={(e) => { setCode(e.target.value) }}></textarea>
       <br />
-      <button className='submit_btn'>Submit</button>
+      <button className='submit_btn' onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
